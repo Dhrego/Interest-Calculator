86147bdc72345ab5509c2e821d04d9c6f5124770
Andrea <andrea.dick3@gmail.com>
2023-09-06 17:12:38 +0200

    managing state

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,14 +1,51 @@
+import { useState } from "react";
+
 const FormInput = (props) => {
+  const defaultInput = {
+    "current-savings": 10000,
+    "yearly-contribution": 1200,
+    "expected-return": 7,
+    duration: 10,
+  };
+
+  const [userInput, setUserInput] = useState(defaultInput);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("popoppo");
+  };
+
+  const resetHandler = () => {
+    setUserInput(defaultInput);
+  };
+
+  const inputHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return { ...prevInput, [input]: value };
+    });
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            onChange={(e) => inputHandler("current-savings", e.target.value)}
+            type="number"
+            id="current-savings"
+            value={userInput["current-savings"]}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            onChange={(e) =>
+              inputHandler("yearly-contribution", e.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+            value={userInput["yearly-contribution"]}
+          />
         </p>
       </div>
       <div className="input-group">
@@ -16,18 +53,28 @@ const FormInput = (props) => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            onChange={(e) => inputHandler("expected-return", e.target.value)}
+            type="number"
+            id="expected-return"
+            value={userInput["expected-return"]}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            onChange={(e) => inputHandler("duration", e.target.value)}
+            type="number"
+            id="duration"
+            value={userInput["duration"]}
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" onClick={resetHandler} className="buttonAlt">
           Reset
         </button>
-        <button onClick={props.onSubmit} type="submit" className="button">
+        <button type="submit" className="button">
           Calculate
         </button>
       </p>
